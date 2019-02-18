6a92d72cd575803964d515bcf4d601cad69e1cee
1768181 <47192783+1768181@users.noreply.github.com>
2019-02-17 21:52:11 -0600

    Update ejercicio3.ml

--- ejercicio3.ml
+++ ejercicio3.ml
@@ -1,11 +1,9 @@
->> syms x
->> Sol=2*exp(x)+3*cos(x)
- 
-Sol =
- 
-3*cos(x) + 2*exp(x)
- 
->> y=solve(Sol,x)
-y =
- 
--1.6936679094546100269188394124169
+%Script para resolver un sistema de ecuaciones
+clc
+clear
+
+syms x
+
+Sol=2*exp(x)+3*cos(x)
+
+y=solve(Sol,x)
