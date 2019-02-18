4dcafe026e33715850f8c86476181274c27568d9
1768181 <47192783+1768181@users.noreply.github.com>
2019-02-17 21:49:32 -0600

    Update ejercicio1.ml

--- ejercicio1.ml
+++ ejercicio1.ml
@@ -1,12 +1,9 @@
->> syms x a
->> Sol=2*x+a-5
- 
-Sol =
- 
-a + 2*x - 5
- 
->> solve(Sol,x)
- 
-ans =
- 
-5/2 - a/2
+%Script para resolver un sistema de ecuaciones
+clc
+clear
+
+syms x a
+
+Sol=2*x+a-5
+
+solve(Sol,x)
